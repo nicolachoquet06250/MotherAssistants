c34fa3e0c55447ce4cdc6bd47bb4ab46abdb04b6
nicolas <nicolachoquet06250@gmail.com>
2019-05-25 19:33:30 +0200

    first commit with repository system and generation system.

--- modules/database/mongodb_connector.js
+++ modules/database/mongodb_connector.js
@@ -4,14 +4,14 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
 // Database Name
-const dbName = 'assmats';
+const dbName = require('./bdd_prefix') + 'assmats';
 
 // Create a new MongoClient
 const client = new MongoClient(url, { useNewUrlParser: true });
 
 const onMongoConnect = (s_callback, e_callback = null) => {
 	client.connect(err => err === null ? s_callback(client) : (e_callback !== null ? e_callback(err) : null));
-}
+};
 
 module.exports = {
 	MongoClient,
