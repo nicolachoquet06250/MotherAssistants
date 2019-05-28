ce94c539e17ed486a3595c76245b3fab4425395c
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-05-28 16:32:45 +0200

    add pre and post install script into package.json and files into commands directory

--- commands/postinstall.js
+++ commands/postinstall.js
@@ -2,5 +2,5 @@ let exec = require('child_process').exec;
 let fs = require('fs');
 
 if(!fs.existsSync(`${__dirname}/../public/uploads`)) {
-    exec("cd ../public && ln -s ../uploads/ .");
+    exec(`cd ${__dirname}/../public && ln -s ../uploads/ .`);
 }
